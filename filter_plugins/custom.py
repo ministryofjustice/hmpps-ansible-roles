2a111a7f74e7b0d43614bbf93ad3ce29a456b99b
Brett Minnie <github@bdb-studios.co.uk>
2018-11-15 14:59:37 +0000

    Added in merge config dicts

--- filter_plugins/custom.py
+++ filter_plugins/custom.py
@@ -2,15 +2,49 @@ from ansible import errors
 
 
 def strip(string, chars):
+    """
+    :param string: string to remove chars from
+    :param chars: characters to remove as string constant
+    :return: string
+    """
     return string.strip(chars)
 
 
 def rstrip(string, chars):
+    """
+    :param string: string to remove chars from
+    :param chars: characters to remove as string constant
+    :return: string
+    """
     return string.rstrip(chars)
 
 
 def lstrip(string, chars):
+    """
+    :param string: string to remove chars from
+    :param chars: characters to remove as string constant
+    :return: string
+    """
     return string.lstrip(chars)
+
+
+def merge_config_dictionaries(*dicts):
+    """
+    Merges n dictionaries of configuration data
+    :param list<dicts>:
+    :return dict:
+    """
+    res_dict = {}
+
+    if isinstance(dicts, list):
+        if len(dicts) == 1 and isinstance(dicts[0], dict):
+            return dicts[0]
+        else:
+            for dictionary in dicts:
+                if isinstance(dictionary, dict):
+                    res_dict.update(dictionary)
+
+    return res_dict
 
 
 class FilterModule(object):
@@ -19,6 +53,7 @@ class FilterModule(object):
         filter_list = {
             'strip': strip,
             'rstrip': rstrip,
-            'lstrip': lstrip
+            'lstrip': lstrip,
+            'merge_config_dictionaries': merge_config_dictionaries
         }
         return filter_list
