565d3bd2fdc0a8287f15c93f526558e4b75f9d02
Brett Minnie <github@bdb-studios.co.uk>
2018-11-16 10:16:30 +0000

    Updated merge method

--- filter_plugins/custom.py
+++ filter_plugins/custom.py
@@ -33,18 +33,13 @@ def merge_config_dictionaries(*dicts):
     Merges n dictionaries of configuration data
     :param list<dicts>:
     :return dict:
+    Merges n dictionaries passed in as arguments, dictionaries will be overwritten in FIFO order
     """
-    res_dict = {}
-
-    if isinstance(dicts, list):
-        if len(dicts) == 1 and isinstance(dicts[0], dict):
-            return dicts[0]
-        else:
-            for dictionary in dicts:
-                if isinstance(dictionary, dict):
-                    res_dict.update(dictionary)
-
-    return res_dict
+    try:
+        return {k: v for d in dicts for k, v in d.items()}
+    except AttributeError as exep:
+        print("Only dictionaries are supported as an argument list")
+        print(exep)
 
 
 class FilterModule(object):
